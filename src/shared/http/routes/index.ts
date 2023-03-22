338aee50378cdcf4f915550c2f075a469d4bede5
Antonio Alves <antoniodecastroalves@gmail.com>
2023-03-22 14:50:39 -0300

    :sparkles: feat: 46 - Authentication Session Route Created

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import productRouter from '@modules/products/routes/products.routes';
 import usersRouter from '@modules/users/routes/users.routes';
+import sessionsRouter from '@modules/users/routes/sessions.routes';
 
 const routes = Router();
 
 routes.use('/products', productRouter);
 routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
