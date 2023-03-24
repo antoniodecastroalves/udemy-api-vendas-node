0ba9a81fcac204d9f8dae68578b97c179d60e90c
Antonio Alves <antoniodecastroalves@gmail.com>
2023-03-24 16:34:24 -0300

    :sparkles: feat: 53 - Created User Avatar Controller

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -21,7 +21,7 @@ class UpdateUserAvatarService {
       throw new AppError(`User not found.`);
     }
 
-    if (!user.avatar) {
+    if (user.avatar) {
       const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
       const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
 
