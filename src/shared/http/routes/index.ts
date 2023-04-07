ad5fceb647a28bf54a7446fc65891e5936a18209
Antonio Alves <antoniodecastroalves@gmail.com>
2023-04-06 23:02:11 -0300

    :recycle: refactor: 82 - Created Customer Routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,16 +1,18 @@
 import { Router } from 'express';
-import productRouter from '@modules/products/routes/products.routes';
+import productsRouter from '@modules/products/routes/products.routes';
 import usersRouter from '@modules/users/routes/users.routes';
 import sessionsRouter from '@modules/users/routes/sessions.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
 import profileRouter from '@modules/users/routes/profile.routes';
+import customersRouter from '@modules/customers/routes/customers.routes';
 
 const routes = Router();
 
-routes.use('/products', productRouter);
+routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
 routes.use('/profile', profileRouter);
+routes.use('/customers', customersRouter);
 
 export default routes;
