0f5897605c30dc150dcfc5fdd3479787ae6d7536
Antonio Alves <antoniodecastroalves@gmail.com>
2023-04-09 23:46:34 -0300

    :sparkles: feat: 90 - Created Service to Create Purchase Orders - part 1

---
+++ src/modules/orders/services/CreateOrderService.ts
@@ -0,0 +1,32 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Order from '../typeorm/entities/Order';
+import OrdersRepository from '../typeorm/repositories/OrdersRepository';
+import CustomersRepository from '@modules/customers/typeorm/repositories/CustomersRepository';
+import ProductRepository from '@modules/products/typeorm/repositories/ProductsRepository';
+
+interface IProduct {
+  id: string;
+  quantity: number;
+}
+
+interface IRequest {
+  customer_id: string;
+  products: IProduct[];
+}
+
+class CreateOrderService {
+  public async execute({ customer_id, products }: IRequest): Promise<Order> {
+    const ordersRepository = getCustomRepository(OrdersRepository);
+    const customersRepository = getCustomRepository(CustomersRepository);
+    const productsRepository = getCustomRepository(ProductRepository);
+
+    const customerExists = await customersRepository.findById(customer_id);
+
+    if (!customerExists) {
+      throw new AppError('Could not find any customer with the given id.');
+    }
+  }
+}
+
+export default CreateOrderService;
