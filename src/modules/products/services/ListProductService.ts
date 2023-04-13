ce8802111441622b99c5efea2b00819336e45916
Antonio Alves <antoniodecastroalves@gmail.com>
2023-04-13 17:54:23 -0300

    :sparkles: feat: 104 - Created Implementing the RedisCache Class - part 2

--- src/modules/products/services/ListProductService.ts
+++ src/modules/products/services/ListProductService.ts
@@ -1,12 +1,17 @@
 import { getCustomRepository } from 'typeorm';
 import Product from '../typeorm/entities/Product';
 import ProductRepository from '../typeorm/repositories/ProductsRepository';
+import RedisCache from '@shared/cache/RedisCache';
 
 class ListProductService {
   public async execute(): Promise<Product[]> {
     const productsRepository = getCustomRepository(ProductRepository);
 
+    const redisCache = new RedisCache();
+
     const products = productsRepository.find();
+
+    await redisCache.save('teste', 'teste');
 
     return products;
   }
