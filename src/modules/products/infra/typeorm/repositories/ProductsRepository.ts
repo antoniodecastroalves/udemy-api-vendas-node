92138a5e447bb8795e0f47bd0ca4f1477777c25e
Antonio Alves <antoniodecastroalves@gmail.com>
2023-04-25 19:59:17 -0300

    :recycle: refactor: Created Domain Infra - Repositories Product

--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -1,33 +1,97 @@
-import { EntityRepository, Repository, In } from 'typeorm';
-import Product from '@modules/products/infra/typeorm/entities/Product';
+import { In, Repository } from 'typeorm';
+import { IProductsRepository } from '@modules/products/domain/repositories/IProductsRepository';
+import Product from '../entities/Product';
+import { IFindProducts } from '@modules/products/domain/models/IFindProducts';
+import { ICreateProduct } from '@modules/products/domain/models/ICreateProduct';
+import { IUpdateStockProduct } from '@modules/products/domain/models/IUpdateStockProduct';
+import { IProductPaginate } from '@modules/products/domain/models/IProductPaginate';
+import { dataSource } from '@shared/infra/typeorm';
 
-interface IFindProducts {
-  id: string;
-}
+type SearchParams = {
+  page: number;
+  skip: number;
+  take: number;
+};
 
-@EntityRepository(Product)
-class ProductRepository extends Repository<Product> {
-  public async findByName(name: string): Promise<Product | undefined> {
-    const product = this.findOne({
-      where: {
-        name,
-      },
+class ProductsRepository implements IProductsRepository {
+  private ormRepository: Repository<Product>;
+
+  constructor() {
+    this.ormRepository = dataSource.getRepository(Product);
+  }
+
+  public async create({
+    name,
+    price,
+    quantity,
+  }: ICreateProduct): Promise<Product> {
+    const product = this.ormRepository.create({ name, price, quantity });
+
+    await this.ormRepository.save(product);
+
+    return product;
+  }
+
+  public async save(product: Product): Promise<Product> {
+    await this.ormRepository.save(product);
+
+    return product;
+  }
+
+  public async remove(product: Product): Promise<void> {
+    await this.ormRepository.remove(product);
+  }
+
+  public async updateStock(products: IUpdateStockProduct[]): Promise<void> {
+    await this.ormRepository.save(products);
+  }
+
+  public async findByName(name: string): Promise<Product | null> {
+    const product = this.ormRepository.findOneBy({
+      name,
     });
 
     return product;
   }
 
-  public async findAllByIds(products: IFindProducts[]): Promise<Product[]> {
-    const productIds = products.map(products => products.id);
+  public async findById(id: string): Promise<Product | null> {
+    const product = this.ormRepository.findOneBy({ id });
 
-    const existsProducts = await this.find({
+    return product;
+  }
+
+  public async findAll({
+    page,
+    skip,
+    take,
+  }: SearchParams): Promise<IProductPaginate> {
+    const [products, count] = await this.ormRepository
+      .createQueryBuilder()
+      .skip(skip)
+      .take(take)
+      .getManyAndCount();
+
+    const result = {
+      per_page: take,
+      total: count,
+      current_page: page,
+      data: products,
+    };
+
+    return result;
+  }
+
+  public async findAllByIds(products: IFindProducts[]): Promise<Product[]> {
+    const productIds = products.map(product => product.id);
+
+    const existentProducts = await this.ormRepository.find({
       where: {
         id: In(productIds),
       },
     });
 
-    return existsProducts;
+    return existentProducts;
   }
 }
 
-export default ProductRepository;
+export default ProductsRepository;
