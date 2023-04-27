58c2aad210e9f798470abc31374ff836528fff0c
Antonio Alves <antoniodecastroalves@gmail.com>
2023-04-26 23:59:29 -0300

    :recycle: refactor: Created Infra Layer - typeorm

--- src/modules/orders/infra/typeorm/repositories/OrdersRepository.ts
+++ src/modules/orders/infra/typeorm/repositories/OrdersRepository.ts
@@ -1,35 +1,60 @@
-import { EntityRepository, Repository } from 'typeorm';
-import Order from '@modules/orders/infra/typeorm/entities/Order';
-import Customer from '@modules/customers/infra/typeorm/entities/Customer';
+import { Repository } from 'typeorm';
+import Order from '../entities/Order';
+import { ICreateOrder } from '@modules/orders/domain/models/ICreateOrder';
+import { IOrdersRepository } from '@modules/orders/domain/repositories/IOrdersRepository';
+import { IOrderPaginate } from '@modules/orders/domain/models/IOrderPaginate';
+import { dataSource } from '@shared/infra/typeorm';
 
-interface IProduct {
-  product_id: string;
-  price: number;
-  quantity: number;
-}
+type SearchParams = {
+  page: number;
+  skip: number;
+  take: number;
+};
 
-interface IRequest {
-  customer: Customer;
-  products: IProduct[];
-}
+class OrdersRepository implements IOrdersRepository {
+  private ormRepository: Repository<Order>;
 
-@EntityRepository(Order)
-class OrdersRepository extends Repository<Order> {
-  public async findById(id: string): Promise<Order | undefined> {
-    const order = this.findOne(id, {
+  constructor() {
+    this.ormRepository = dataSource.getRepository(Order);
+  }
+
+  public async findById(id: string): Promise<Order | null> {
+    const order = this.ormRepository.findOne({
+      where: { id },
       relations: ['order_products', 'customer'],
     });
 
     return order;
   }
 
-  public async createOrder({ customer, products }: IRequest): Promise<Order> {
-    const order = this.create({
+  public async findAll({
+    page,
+    skip,
+    take,
+  }: SearchParams): Promise<IOrderPaginate> {
+    const [orders, count] = await this.ormRepository
+      .createQueryBuilder()
+      .skip(skip)
+      .take(take)
+      .getManyAndCount();
+
+    const result = {
+      per_page: take,
+      total: count,
+      current_page: page,
+      data: orders,
+    };
+
+    return result;
+  }
+
+  public async create({ customer, products }: ICreateOrder): Promise<Order> {
+    const order = this.ormRepository.create({
       customer,
       order_products: products,
     });
 
-    await this.save(order);
+    await this.ormRepository.save(order);
 
     return order;
   }
