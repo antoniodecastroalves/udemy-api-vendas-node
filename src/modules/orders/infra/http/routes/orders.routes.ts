d10bdb26833b05d0d20afc955034efa527dec644
Antonio Alves <antoniodecastroalves@gmail.com>
2023-04-26 23:45:02 -0300

    :recycle: refactor: Created Infra Layer - routes

--- src/modules/orders/infra/http/routes/orders.routes.ts
+++ src/modules/orders/infra/http/routes/orders.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import { celebrate, Joi, Segments } from 'celebrate';
-import OrdersController from '@modules/orders/infra/http/controllers/OrdersController';
+import OrdersController from '../controllers/OrdersController';
 import isAuthenticated from '@shared/infra/http/middlewares/isAuthenticated';
 
 const ordersRouter = Router();
 const ordersController = new OrdersController();
 
 ordersRouter.use(isAuthenticated);
+
+ordersRouter.get('/', ordersController.index);
 
 ordersRouter.get(
   '/:id',
