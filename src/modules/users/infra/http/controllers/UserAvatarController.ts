e25b6e6318900494f591364e8daac524a36c7acb
Antonio Alves <antoniodecastroalves@gmail.com>
2023-04-28 22:07:53 -0300

    :recycle: refactor: Created Infra Layer - UserAvatarController

--- src/modules/users/infra/http/controllers/UserAvatarController.ts
+++ src/modules/users/infra/http/controllers/UserAvatarController.ts
@@ -1,16 +1,17 @@
 import { Request, Response } from 'express';
+import { container } from 'tsyringe';
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
-import { instanceToInstance } from 'class-transformer';
+import { classToClass } from 'class-transformer';
 
 export default class UserAvatarController {
   public async update(request: Request, response: Response): Promise<Response> {
-    const updateAvatar = new UpdateUserAvatarService();
+    const updateAvatar = container.resolve(UpdateUserAvatarService);
 
-    const user = updateAvatar.execute({
-      user_id: request.user.id as string,
-      avatarFilename: request.file?.filename as string,
+    const user = await updateAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
     });
 
-    return response.json(instanceToInstance(user));
+    return response.json(classToClass(user));
   }
 }
