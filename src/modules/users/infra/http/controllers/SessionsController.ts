396eb4dee6612579e32b61481be2b34ed3d615b2
Antonio Alves <antoniodecastroalves@gmail.com>
2023-04-28 22:06:17 -0300

    :recycle: refactor: Created Infra Layer - SessionsController

--- src/modules/users/infra/http/controllers/SessionsController.ts
+++ src/modules/users/infra/http/controllers/SessionsController.ts
@@ -1,18 +1,19 @@
 import { Request, Response } from 'express';
+import { container } from 'tsyringe';
 import CreateSessionsService from '@modules/users/services/CreateSessionsService';
-import { instanceToInstance } from 'class-transformer';
+import { classToClass } from 'class-transformer';
 
 export default class SessionsController {
   public async create(request: Request, response: Response): Promise<Response> {
     const { email, password } = request.body;
 
-    const createSession = new CreateSessionsService();
+    const createSession = container.resolve(CreateSessionsService);
 
     const user = await createSession.execute({
       email,
       password,
     });
 
-    return response.json(instanceToInstance(user));
+    return response.json(classToClass(user));
   }
 }
