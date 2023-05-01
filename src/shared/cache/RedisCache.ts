c06a854f968855a9056e2f16a134faf362e59846
Antonio Alves <antoniodecastroalves@gmail.com>
2023-05-01 18:18:42 -0300

    :recycle: refactor: Created Share Layer - RedisCache

--- src/shared/cache/RedisCache.ts
+++ src/shared/cache/RedisCache.ts
@@ -1,29 +1,36 @@
 import Redis, { Redis as RedisClient } from 'ioredis';
 import cacheConfig from '@config/cache';
 
-export default class RedisCache {
-  private cliente: RedisClient;
+class RedisCache {
+  private client: RedisClient;
+  private connected = false;
 
   constructor() {
-    this.cliente = new Redis(cacheConfig.config.redis);
+    if (!this.connected) {
+      this.client = new Redis(cacheConfig.config.redis);
+      this.connected = true;
+    }
   }
 
   public async save(key: string, value: any): Promise<void> {
-    await this.cliente.set(key, JSON.stringify(value));
+    await this.client.set(key, JSON.stringify(value));
   }
 
   public async recover<T>(key: string): Promise<T | null> {
-    const data = await this.cliente.get(key);
+    const data = await this.client.get(key);
 
     if (!data) {
       return null;
     }
+
     const parsedData = JSON.parse(data) as T;
 
     return parsedData;
   }
 
   public async invalidate(key: string): Promise<void> {
-    await this.cliente.del(key);
+    await this.client.del(key);
   }
 }
+
+export default new RedisCache();
