b60f6589fe9c4f1bd96644889a27332b322785d5
Antonio Alves <antoniodecastroalves@gmail.com>
2023-05-01 18:23:22 -0300

    :recycle: refactor: Created Share Layer - middlewares rateLimiter

--- src/shared/infra/http/middlewares/rateLimiter.ts
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -1,7 +1,20 @@
 import { Request, Response, NextFunction } from 'express';
-import Redis from 'ioredis';
+import redis from 'redis';
 import { RateLimiterRedis } from 'rate-limiter-flexible';
 import AppError from '@shared/errors/AppError';
+
+const redisClient = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+  password: process.env.REDIS_PASS || undefined,
+});
+
+const limiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: 'ratelimit',
+  points: 5,
+  duration: 1,
+});
 
 export default async function rateLimiter(
   request: Request,
@@ -9,19 +22,6 @@ export default async function rateLimiter(
   next: NextFunction,
 ): Promise<void> {
   try {
-    const redisClient = new Redis({
-      host: process.env.REDIS_HOST,
-      port: Number(process.env.REDIS_PORT),
-      password: process.env.REDIS_PASS || undefined,
-    });
-
-    const limiter = new RateLimiterRedis({
-      storeClient: redisClient,
-      keyPrefix: 'ratelimit',
-      points: 5,
-      duration: 1,
-    });
-
     await limiter.consume(request.ip);
 
     return next();
