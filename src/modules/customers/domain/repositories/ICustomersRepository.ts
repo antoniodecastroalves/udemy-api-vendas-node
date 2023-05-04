9648a530116add2e2fe0bfbc137fc9777a594acd
Antonio Alves <antoniodecastroalves@gmail.com>
2023-05-03 21:19:07 -0300

    :recycle: refactor: Created Domain Layer - ICustomersRepository

--- src/modules/customers/domain/repositories/ICustomersRepository.ts
+++ src/modules/customers/domain/repositories/ICustomersRepository.ts
@@ -7,11 +7,12 @@ export type SearchParams = {
   skip: number;
   take: number;
 };
+
 export interface ICustomersRepository {
   findAll({ page, skip, take }: SearchParams): Promise<ICustomerPaginate>;
-  findByName(name: string): Promise<ICustomer | undefined>;
-  findById(id: string): Promise<ICustomer | undefined>;
-  findByEmail(email: string): Promise<ICustomer | undefined>;
+  findByName(name: string): Promise<ICustomer | null>;
+  findById(id: string): Promise<ICustomer | null>;
+  findByEmail(email: string): Promise<ICustomer | null>;
   create(data: ICreateCustomer): Promise<ICustomer>;
   save(customer: ICustomer): Promise<ICustomer>;
   remove(customer: ICustomer): Promise<void>;
